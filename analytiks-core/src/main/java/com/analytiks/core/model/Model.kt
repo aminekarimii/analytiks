c2ca453592cfe608a41ccc4c70109c3bf28bc970
Amine karimi <aminekarimi1998@gmail.com>
2023-02-13 01:45:59 +0100

    group models to a single file

--- analytiks-core/src/main/java/com/analytiks/core/model/Model.kt
+++ analytiks-core/src/main/java/com/analytiks/core/model/Model.kt
@@ -1,5 +1,11 @@
 package com.analytiks.core.model
 
+
+data class UserProperty(
+    val propertyName: String,
+    val propertyValue: Any?
+)
+
 data class EventProperty(
     val propertyName: String,
     val propertyValue: String,
