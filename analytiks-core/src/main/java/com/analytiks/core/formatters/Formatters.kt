13bd36addf1858e971364aec96df4f1eeb52bd5e
Amine <aminekarimi1998@gmail.com>
2023-03-01 23:47:46 +0100

    rename EventProperty to Param

--- analytiks-core/src/main/java/com/analytiks/core/formatters/Formatters.kt
+++ analytiks-core/src/main/java/com/analytiks/core/formatters/Formatters.kt
@@ -1,18 +1,18 @@
 package com.analytiks.core.formatters
 
-import com.analytiks.core.model.EventProperty
+import com.analytiks.core.model.Param
 import org.json.JSONObject
 
 abstract class FormatEventPropertiesStrategy<T> {
-    operator fun invoke(vararg eventProperties: EventProperty?): T {
+    operator fun invoke(vararg eventProperties: Param?): T {
         return this.formatProperties(*eventProperties)
     }
 
-    abstract fun formatProperties(vararg eventProperties: EventProperty?): T
+    abstract fun formatProperties(vararg eventProperties: Param?): T
 }
 
 class JSONFormatStrategy : FormatEventPropertiesStrategy<JSONObject?>() {
-    override fun formatProperties(vararg eventProperties: EventProperty?): JSONObject {
+    override fun formatProperties(vararg eventProperties: Param?): JSONObject {
         val mapProps = mutableMapOf<String?, Any?>()
         eventProperties.forEach {
             mapProps[it?.propertyName] = it?.propertyValue
@@ -23,7 +23,7 @@ class JSONFormatStrategy : FormatEventPropertiesStrategy<JSONObject?>() {
 }
 
 class MapFormatStrategy : FormatEventPropertiesStrategy<Map<String, String>>() {
-    override fun formatProperties(vararg eventProperties: EventProperty?): Map<String, String> {
+    override fun formatProperties(vararg eventProperties: Param?): Map<String, String> {
         return eventProperties.associate { property ->
             property!!.propertyName to property.propertyName
         }
