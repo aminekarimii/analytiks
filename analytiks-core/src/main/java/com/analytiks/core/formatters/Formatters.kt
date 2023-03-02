7a7ae51bf248d71719a2fbe6f7c3daab42e256ba
Amine Karimi <amine.karimi@neoxia.com>
2023-03-02 15:45:57 +0100

    remove result nullability of the formatters

--- analytiks-core/src/main/java/com/analytiks/core/formatters/Formatters.kt
+++ analytiks-core/src/main/java/com/analytiks/core/formatters/Formatters.kt
@@ -4,18 +4,18 @@ import com.analytiks.core.model.Param
 import org.json.JSONObject
 
 abstract class FormatEventPropertiesStrategy<T> {
-    operator fun invoke(vararg eventProperties: Param?): T {
+    operator fun invoke(vararg eventProperties: Param): T {
         return this.formatProperties(*eventProperties)
     }
 
-    abstract fun formatProperties(vararg eventProperties: Param?): T
+    abstract fun formatProperties(vararg eventProperties: Param): T
 }
 
-class JSONFormatStrategy : FormatEventPropertiesStrategy<JSONObject?>() {
-    override fun formatProperties(vararg eventProperties: Param?): JSONObject {
+class JSONFormatStrategy : FormatEventPropertiesStrategy<JSONObject>() {
+    override fun formatProperties(vararg eventProperties: Param): JSONObject {
         val mapProps = mutableMapOf<String?, Any?>()
         eventProperties.forEach {
-            mapProps[it?.propertyName] = it?.propertyValue
+            mapProps[it.propertyName] = it.propertyValue
         }
 
         return JSONObject(mapProps)
@@ -23,9 +23,9 @@ class JSONFormatStrategy : FormatEventPropertiesStrategy<JSONObject?>() {
 }
 
 class MapFormatStrategy : FormatEventPropertiesStrategy<Map<String, String>>() {
-    override fun formatProperties(vararg eventProperties: Param?): Map<String, String> {
+    override fun formatProperties(vararg eventProperties: Param): Map<String, String> {
         return eventProperties.associate { property ->
-            property!!.propertyName to property.propertyName
+            property.propertyName to property.propertyName
         }
     }
 }
