dd5bb792649cf8cce70f5fbf9a90305408cfce2a
Amine Karimi <aminekarimi1998@gmail.com>
2023-03-08 18:03:19 +0100

    remove addon module and rename analytiks addons

---
+++ addon/analytiks-mixpanel/src/main/java/com/analytiks/addon/mixpanel/MixpanelAnalyticsClient.kt
@@ -0,0 +1,54 @@
+package com.analytiks.addon.mixpanel
+
+import android.content.Context
+import android.util.Log
+import com.analytiks.core.CoreAddon
+import com.analytiks.core.EventsExtension
+import com.analytiks.core.UserProfileExtension
+import com.analytiks.core.formatters.JSONFormatStrategy
+import com.analytiks.core.model.Param
+import com.analytiks.core.model.UserProperty
+import com.mixpanel.android.mpmetrics.MixpanelAPI
+import org.json.JSONObject
+
+
+const val TAG = "AnalyticsClient"
+
+class MixpanelAnalyticsClient(
+    private val token: String,
+    private val optOutTrackingDefault: Boolean = false,
+    private val superProperties: JSONObject? = null,
+    private val instanceName: String? = null,
+    private val trackAutomaticEvents: Boolean = true,
+) : CoreAddon, EventsExtension, UserProfileExtension {
+
+    private lateinit var mixpanelClient: MixpanelAPI
+
+    override fun initialize(context: Context) {
+        mixpanelClient = MixpanelAPI.getInstance(
+            context,
+            this.token,
+            this.optOutTrackingDefault,
+            this.superProperties,
+            this.instanceName,
+            this.trackAutomaticEvents,
+        )
+    }
+
+    override fun logEvent(name: String) {
+        mixpanelClient.track(name)
+    }
+
+    override fun logEvent(name: String, vararg properties: Param) {
+        val formattedProps = JSONFormatStrategy().invoke(*properties)
+        mixpanelClient.track(name, formattedProps)
+    }
+
+    override fun identify(userId: String?) {
+        Log.d(TAG, " Mixpanel identified")
+    }
+
+    override fun setUserProperty(property: UserProperty) {
+        Log.d(TAG, " Mixpanel Property Set")
+    }
+}
