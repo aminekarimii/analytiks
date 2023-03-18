0bcedb2612ca15a8a99cbbb9115cc9ebbd9dd104
driouecheMed <drioueche.med@gmail.com>
2023-03-18 12:14:43 +0100

    Add AnalytiksFactory

--- app/src/main/java/com/logitanalyticsapp/di/AppContainer.kt
+++ app/src/main/java/com/logitanalyticsapp/di/AppContainer.kt
@@ -1,14 +1,13 @@
 package com.logitanalyticsapp.di
 
 import com.analytiks.Analytiks
-import com.analytiks.addon.mixpanel.MixpanelAnalyticsClient
-import com.analytiks.addon.timber.TimberLocalClient
+import com.analytiks.AnalytiksFactory
 import com.analytiks.core.CoreAddon
 
 class AppContainer {
     private val clients: List<CoreAddon> = listOf(
-        TimberLocalClient(),
-        MixpanelAnalyticsClient(
+        AnalytiksFactory.createTimberLocalClient(),
+        AnalytiksFactory.createMixpanelAnalyticsClient(
             token = "test-key-goes-here",
             optOutTrackingDefault = true,
         ),
