5363d43ad6615a1284bd520b9fe38646358e7297
Amine karimi <aminekarimi1998@gmail.com>
2024-02-27 00:06:26 +0100

    Code formating

--- addon/analytiks-appvisor/src/main/java/com/analytiks/addon/appvisor/ui/AppVisorDataCollector.kt
+++ addon/analytiks-appvisor/src/main/java/com/analytiks/addon/appvisor/ui/AppVisorDataCollector.kt
@@ -2,7 +2,6 @@ package com.analytiks.addon.appvisor.ui
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.analytiks.core.EventLog
 import com.analytiks.core.VisorEvent
 
 class AppVisorDataCollector private constructor() {
@@ -19,10 +18,10 @@ class AppVisorDataCollector private constructor() {
 
     companion object {
         @Volatile
-        private var instance: AppVisorDataCollector? = null // Volatile modifier is necessary
+        private var instance: AppVisorDataCollector? = null
 
         fun getInstance() =
-            instance ?: synchronized(this) { // synchronized to avoid concurrency problem
+            instance ?: synchronized(this) {
                 instance ?: AppVisorDataCollector().also { instance = it }
             }
     }
